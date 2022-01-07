30bb0844ebed8e0a090a5246f334825f97978f77
kasem32833 <abulkasem32833@gmail.com>
2022-01-07 11:20:35 +0300

    Complete up to class-2 styling with tailwindcss creating nav with logo and heroa area

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,18 @@
-this is a header
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
+    <div class="min-w-max">
+        <img width="100" src="../img/logo.jpeg" alt="">
+    </div>
+    <div class="w-full ">
+        <ul class= "flex justify-center">
+            <li><a class="inline-block p-4 text-white" href="">Land</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Villa</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Apartment</a></li>
+            <li><a class="inline-block p-4 text-white" href="">About Us</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="min-w-max text-2xl">
+        <a href="">🇹🇷</a>
+        <a href="">🇺🇸</a>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-guest-layout>
-    test
+    <div style="background-image:url(../img/hero-bg.jpeg)" class="relative pt-52 pb-64 z-10 bg-center bg-cover bg-no-repeat">
+        <div class="absolute h-full w-full bg-black opacity-50 top-0 left-0"></div>
+        <div class="container relative z-20 text-white text-center text-2xl">
+            <h2 class= "font-bold text-4xl mb-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br/> Quisquam, laudantium?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugit.</p>
+        </div>
+    </div>
+   
 </x-guest-layout>
