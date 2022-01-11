da5cc152e248edc5e77c9372648a8c4518b158f8
kasem32833 <abulkasem32833@gmail.com>
2022-01-11 18:17:02 +0300

    using jQuery slim build create model and migration and controller in asingle command

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,4 +8,12 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-jQuery(window).scroll(function)
+jQuery(window).scroll(function() {
+    const scroll = jQuery(window).scrollTop();
+
+    if (scroll >= 50) {
+        jQuery('.sticky-header').addClass('sticky-header-active');
+    }else{
+        jQuery('.sticky-header').removeClass('sticky-header-active');
+    }
+});
