3af53d618cef97e277aa04e32fa701972f38b3d1
kasem32833 <abulkasem32833@gmail.com>
2022-01-11 15:45:46 +0300

    Add jqery slim version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import jQuery from './jquery-3.6.0.slim.min';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+jQuery(window).scroll(function)
